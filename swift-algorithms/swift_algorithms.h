cf017494e95120b06ba2b810ac12dc6e5134cfae
Felipe Weber <weberecomp@gmail.com>
2020-04-07 11:50:12 -0300

    Initial Commit

---
+++ swift-algorithms/swift_algorithms.h
@@ -0,0 +1,19 @@
+//
+//  swift_algorithms.h
+//  swift-algorithms
+//
+//  Created by Felipe Weber on 07/04/20.
+//  Copyright © 2020 Felipe Weber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for swift_algorithms.
+FOUNDATION_EXPORT double swift_algorithmsVersionNumber;
+
+//! Project version string for swift_algorithms.
+FOUNDATION_EXPORT const unsigned char swift_algorithmsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_algorithms/PublicHeader.h>
+
+
